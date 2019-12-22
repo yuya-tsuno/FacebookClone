df83ce7a1a7654bf9d81cca71b70f6cd89463d3a
Tsuno Yuya <2ch.yuya@gmail.com>
2019-12-22 17:48:42 +0900

    タイムゾーンの設定を日本に変更

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module ReFacebookClone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
-
-# config.time_zone = 'Tokyo'
-# config.active_record.default_timezone = :local
