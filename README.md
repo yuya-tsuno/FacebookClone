6ec1fb48a873d377f7abea2626cfb58ec2250b74
Tsuno Yuya <2ch.yuya@gmail.com>
2019-12-22 22:49:51 +0900

    Feedsテーブルにcontentカラムを追加

--- README.md
+++ README.md
@@ -22,4 +22,5 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
 # FacebookClone
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_090341) do
+ActiveRecord::Schema.define(version: 2019_12_22_134759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_22_090341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.text "content"
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
